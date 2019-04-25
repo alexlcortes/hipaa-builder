17efaaec3c2a6315965b74a2b96b0a220320d667
alexlcortes <jando.cortes@gmail.com>
2019-04-25 18:33:29 +0000

    app changes. Need to get data from divs

--- app.js
+++ app.js
@@ -14,21 +14,16 @@ function drop(event) {
   var data = event.dataTransfer.getData("Text");
   event.target.appendChild(document.getElementById(data));
 
-  //Wanted to check what is data
-  console.log("The ID is " + data);
+//Wanted to check what is data
+console.log(data.length);
 
-
-  //console.log(mfa);
-  //document.getElementById("mf").innerHTML = "Multi-Factor has been added";
+//if (data ) {
+//console.log("You got IPS!");
+//} else {
+//console.log("You got something else...");
+//}
 
 //This should print the name under column 3
-  document.getElementById('mult').innerHTML=data;
+//document.getElementById('ips').innerHTML=data;
 
-
-//Trying to select info by checking id of dropped item
-//  if (data = "ips"){
-//    console.log("ips");
-//  } else {
-//    console.log(data);
-//  }
 }
