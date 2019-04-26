5d64c14b3da95c09202498c4ca83eaa486dd4f32
Alex Cortes <acortes@staff.atlantic.net>
2019-04-26 14:19:18 -0400

    Elements will have their own formulas.

--- app.js
+++ app.js
@@ -1,26 +1,52 @@
-var newData = [];
+let newData = [];
+let itemPrice = 0;
 
+//Allow for element to be dragged
 function dragStart(event) {
   event.dataTransfer.setData("Text", event.target.id);
 }
 
+//Allot for element to drop.
 function allowDrop(event) {
   event.preventDefault();
 }
 
+//When element is dropped do this:
 function drop(event) {
   event.preventDefault();
   var data = event.dataTransfer.getData("Text");
   event.target.appendChild(document.getElementById(data));
-  console.log("Data: " + data);
+  console.log("Data/id: " + data);
   newData.push(data);
-  printnewData();
+  itemPrice++;
+}
 
-//How to convert data into integer
-//console.log(parseInt(data, 10) + 20);
+function drop2(event) {
+  event.preventDefault();
+  var data = event.dataTransfer.getData("Text");
+  event.target.appendChild(document.getElementById(data));
+  console.log("Data/id: " + data);
+
+  // Send data to array
+  newData.push(data);
+
+  //add a number to itemPrice varaible
+  itemPrice = itemPrice + 5;
 
 }
 
-function printnewData() {
-  console.log("Array newData: " + newData[0]);
+//function addPrice() {
+  //console.log("Added ")
+//}
+
+//Should produce NaN
+console.log(newData);
+
+//Converts data into integer
+function cal1() {
+  //addPrice();
+  console.log(newData);
+  console.log("Item Price: " + itemPrice);
+  var parsed = parseInt(newData);
+  console.log(parsed);
 }
