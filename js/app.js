225c73ab5b0bfd7868c065e9a371e8a6f0c68563
Alex Cortes <acortes@staff.atlantic.net>
2019-05-03 15:11:12 -0400

    minor modifications

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-let newData = [];
-let itemPrice = 0;
-
 //Allow for element to be dragged
 function dragStart(event) {
   event.dataTransfer.setData("Text", event.target.id);
@@ -17,36 +14,4 @@ function drop(event) {
   var data = event.dataTransfer.getData("Text");
   event.target.appendChild(document.getElementById(data));
   console.log("Data/id: " + data);
-  newData.push(data);
-  itemPrice++;
-}
-
-function drop2(event) {
-  event.preventDefault();
-  var data = event.dataTransfer.getData("Text");
-  event.target.appendChild(document.getElementById(data));
-  console.log("Data/id: " + data);
-
-  // Send data to array
-  newData.push(data);
-
-  //add a number to itemPrice varaible
-  itemPrice = itemPrice + 5;
-
-}
-
-//function addPrice() {
-  //console.log("Added ")
-//}
-
-//Should produce NaN
-console.log(newData);
-
-//Converts data into integer
-function cal1() {
-  //addPrice();
-  console.log(newData);
-  console.log("Item Price: " + itemPrice);
-  var parsed = parseInt(newData);
-  console.log(parsed);
 }
--- js/app2.js
+++ js/app2.js
@@ -1,7 +1,5 @@
-var total = 0;
-
 function drag_handler(ev) {
- console.log("Drag");
+ //console.log("Drag");
 }
 
 function dragstart_handler(ev) {
@@ -11,18 +9,10 @@ function dragstart_handler(ev) {
 
 function drop_handler(ev) {
  console.log("Drop");
- total+5;
  ev.currentTarget.style.background = "lightyellow";
-totalVal()
  ev.preventDefault();
  var data = ev.dataTransfer.getData("text");
  ev.target.appendChild(document.getElementById(data));
-}
-
-function totalVal() {
-  if (total >= 0) {
-    document.getElementById("totals").innerHTML=total;
-  }
 }
 
 function dragover_handler(ev) {
